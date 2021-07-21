8202817b56a69fc6b2143b66a3048d8e7074cff8
Peter Ullrich <dotup.software@gmail.com>
2021-07-21 17:09:22 +0200

    -sqlite -windows

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,17 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:moor_db_viewer_example/my_app.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
-void main() => runApp(MyApp());
+
+void main(){
+  if (Platform.isWindows || Platform.isLinux) {
+    // Initialize FFI
+    sqfliteFfiInit();
+    // Change the default factory
+    databaseFactory = databaseFactoryFfi;
+  }
+   runApp(MyApp());
+}
